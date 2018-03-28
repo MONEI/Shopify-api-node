e562a2cc80967ffd62c3d305b4484b41975dab3e
Luigi Pinca <luigipinca@gmail.com>
2018-03-28 10:23:04 +0200

    [api] Add AccessScope resource (#171)
    Fixes: https://github.com/MONEI/Shopify-api-node/issues/170

---
+++ resources/access-scope.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const assign = require('lodash/assign');
+
+/**
+ * Creates an AccessScope instance.
+ *
+ * @param {Shopify} shopify Reference to the Shopify instance
+ * @constructor
+ * @public
+ */
+function AccessScope(shopify) {
+  this.shopify = shopify;
+
+  this.name = 'access_scopes';
+}
+
+/**
+ * Retrieves a list of access scopes associated to the access token.
+ *
+ * @return {Promise} Promise that resolves with the result
+ * @public
+ */
+AccessScope.prototype.list = function list() {
+  const path = `/admin/oauth/${this.name}.json`;
+  const url = assign({ path }, this.shopify.baseUrl);
+  return this.shopify.request(url, 'GET', this.name);
+};
+
+module.exports = AccessScope;
---
+++ test/access-scope.test.js
@@ -0,0 +1,24 @@
+describe('Shopify#accessScope', () => {
+  'use strict';
+
+  const expect = require('chai').expect;
+
+  const fixtures = require('./fixtures/access-scope');
+  const common = require('./common');
+
+  const shopify = common.shopify;
+  const scope = common.scope;
+
+  afterEach(() => expect(scope.isDone()).to.be.true);
+
+  it('gets a list of access scopes associated to the access token', () => {
+    const output = fixtures.res.list;
+
+    scope
+      .get('/admin/oauth/access_scopes.json')
+      .reply(200, output);
+
+    return shopify.accessScope.list()
+      .then(data => expect(data).to.deep.equal(output.access_scopes));
+  });
+});
