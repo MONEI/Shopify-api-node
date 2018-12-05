dd0f92066317e9d83f136f0ac6470ff0b68133cc
John Barton <john.barton@purenet.co.uk>
2018-12-05 12:21:55 +0000

    Fix tests

--- test/fixtures/recurring-application-charge/res/index.js
+++ test/fixtures/recurring-application-charge/res/index.js
@@ -4,3 +4,4 @@ exports.activate = require('./activate');
 exports.create = require('./create');
 exports.list = require('./list');
 exports.get = require('./get');
+exports.customize = require('./customize');
