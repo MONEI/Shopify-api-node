8b792800d59caadb605bb84420a211b6aff6f261
joshgare <josh@jgapplications.com>
2019-05-20 18:34:20 +0100

    source_name can also be a string containing a sales channel's app id

--- index.d.ts
+++ index.d.ts
@@ -1905,7 +1905,7 @@ declare namespace Shopify {
     shipping_address: ICustomerAddress;
     shipping_lines: IOrderShippingLine[];
     source_identifier: string | null;
-    source_name: 'web' | 'pos' | 'shopify_draft_order' | 'iphone' | 'android';
+    source_name: 'web' | 'pos' | 'shopify_draft_order' | 'iphone' | 'android' | string;
     subtotal_price: string;
     subtotal_price_set: IOrderAdjustmentAmountSet;
     tags: string;
