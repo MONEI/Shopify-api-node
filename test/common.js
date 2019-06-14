03edc9b540313f0c706272fd03d5de1a0739f36a
Isaac Maier <padspambox@gmail.com>
2019-06-14 15:35:01 -0400

    Add tests for API version support

--- test/common.js
+++ test/common.js
@@ -8,6 +8,7 @@ const Shopify = require('..');
 const accessToken = 'f85632530bf277ec9ac6f649fc327f17';
 const password = '72297d971271bc62ca899bba7432acb1';
 const apiKey = 'bc731e500840231da5b43bb3f388d2f0';
+const apiVersion = '2019-04';
 const shopName ='johns-apparel';
 
 const shopify = new Shopify({ shopName, accessToken });
@@ -26,5 +27,6 @@ module.exports = {
   shopName,
   shopify,
   apiKey,
+  apiVersion,
   scope
 };
