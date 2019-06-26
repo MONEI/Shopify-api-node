1cf1817b8020d7110b739e5f48054706611dc524
Dominic Masters <dominic@domsplace.com>
2019-06-27 07:46:21 +1000

    [ts] Add various ICarrier types

--- index.d.ts
+++ index.d.ts
@@ -2833,4 +2833,58 @@ declare namespace Shopify {
     : T extends 'themes/delete'
     ? IDeletedItem
     : any;
+
+  interface ICarrierAddress {
+    country: string;
+    postal_code: string;
+    province?: string;
+    city: string;
+    name?: string;
+    address1?: string;
+    address2?: string;
+    address3?: string;
+    phone?: string;
+    fax?: string;
+    email?: string;
+    address_type?: string;
+    company_name?: string;
+  }
+
+  interface ICarrierItem {
+    name?: string;
+    sku?: string;
+    quantity: number;
+    grams?: number;
+    price: number;
+    vendor?: string;
+    requires_shipping: boolean;
+    taxable: boolean;
+    fulfillment_service?: string;
+    properties?: ILineItemProperty;
+    product_id: number;
+    variant_id: number;
+  }
+
+  interface ICarrierRequestBody {
+    origin: ICarrierAddress;
+    destination: ICarrierAddress;
+    items: ICarrierItem[];
+    currency: string;
+    locale: string;
+  }
+
+  export interface ICarrierRequest {
+    rate: ICarrierRequestBody;
+  }
+
+  export interface ICarrierResponse {
+    service_name: string;
+    description?: string;
+    service_code: string;
+    currency: string;
+    total_price: number;
+    phone_required?: boolean;
+    min_delivery_date?: string;
+    max_delivery_date?: string;
+  }
 }
