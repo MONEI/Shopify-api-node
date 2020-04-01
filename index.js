07308c2d13830825a22a3f6f089e63f6a751df68
mete89 <meteerzincanli@hotmail.com>
2020-04-01 18:28:38 +0300

    [fix] Exit early if the throttle argument is falsy (#366)
    Make `Shopify.prototype.updateGraphqlLimits()` a noop if the throttle
    argument is falsy.

--- index.js
+++ index.js
@@ -185,6 +185,8 @@ Shopify.prototype.request = function request(uri, method, key, data, headers) {
  * @private
  */
 Shopify.prototype.updateGraphqlLimits = function updateGraphqlLimits(throttle) {
+  if (!throttle) return;
+
   const limits = this.callGraphqlLimits;
 
   limits.remaining = throttle.currentlyAvailable;
