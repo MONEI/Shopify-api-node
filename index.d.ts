MONEI/Shopify-api-node

commit eeda19880b634d5080e3ab2cc5b77513276c8dbe

[api] Introduce the hooks option  (#560)
Add support for passing request lifecycle hooks down to `got`.

Fixes: https://github.com/MONEI/Shopify-api-node/issues/556

index.d.ts
@@ -1,6 +1,7 @@
 // Type definitions for shopify-api-node
 // Project: shopify-api-node
 // Definitions by: Rich Buggy <rich@buggy.id.au>
+import { Got } from 'got';
 
 /*~ This is the module template file for class modules.
  *~ You should rename it to index.d.ts and place it in a folder with the same name as the module.
@@ -787,6 +788,7 @@ declare namespace Shopify {
     presentmentPrices?: boolean;
     shopName: string;
     timeout?: number;
+    hooks?: Got.Hooks;
   }
 
   export interface IPrivateShopifyConfig {
@@ -798,6 +800,7 @@ declare namespace Shopify {
     presentmentPrices?: boolean;
     shopName: string;
     timeout?: number;
+    hooks?: Got.Hooks;
   }
 
   export interface ICallLimits {

index.js
@@ -154,31 +154,41 @@ Shopify.prototype.request = function request(uri, method, key, data, headers) {
     parseJson: this.options.parseJson,
     timeout: this.options.timeout,
     responseType: 'json',
-    retry:
-      this.options.maxRetries > 0
-        ? {
-            calculateDelay,
-            errorCodes: retryableErrorCodesArray,
-            limit: this.options.maxRetries,
-            // Don't clamp Shopify `Retry-After` header values too low.
-            maxRetryAfter: Infinity,
-            methods: [method],
-            statusCodes: retryableStatusCodesArray
-          }
-        : 0,
-    method,
-    hooks: {
-      afterResponse: [
-        (res) => {
-          this.updateLimits(res.headers['x-shopify-shop-api-call-limit']);
-          return res;
-        }
-      ]
-    }
+    method
   };
+
+  const afterResponse = (res) => {
+    this.updateLimits(res.headers['x-shopify-shop-api-call-limit']);
+    return res;
+  };
+
+  if (this.options.hooks) {
+    options.hooks = { ...this.options.hooks };
+    options.hooks.afterResponse = [afterResponse];
+
+    if (this.options.hooks.afterResponse) {
+      options.hooks.afterResponse.push(...this.options.hooks.afterResponse);
+    }
+  } else {
+    options.hooks = { afterResponse: [afterResponse] };
+  }
 
   if (data) {
     options.json = key ? { [key]: data } : data;
+  }
+
+  if (this.options.maxRetries > 0) {
+    options.retry = {
+      calculateDelay,
+      errorCodes: retryableErrorCodesArray,
+      limit: this.options.maxRetries,
+      // Don't clamp Shopify `Retry-After` header values too low.
+      maxRetryAfter: Infinity,
+      methods: [method],
+      statusCodes: retryableStatusCodesArray
+    };
+  } else {
+    options.retry = 0;
   }
 
   return got(uri, options).then((res) => {
@@ -272,39 +282,56 @@ Shopify.prototype.graphql = function graphql(data, variables) {
     timeout: this.options.timeout,
     responseType: 'json',
     method: 'POST',
-    body: json ? this.options.stringifyJson({ query: data, variables }) : data,
-    retry:
-      this.options.maxRetries > 0
-        ? {
-            calculateDelay,
-            errorCodes: retryableErrorCodesArray,
-            limit: this.options.maxRetries,
-            // Don't clamp Shopify `Retry-After` header values too low.
-            maxRetryAfter: Infinity,
-            methods: ['POST'],
-            statusCodes: retryableStatusCodesArray
-          }
-        : 0,
-    hooks: {
-      afterResponse: [
-        (res) => {
-          if (res.body) {
-            if (res.body.extensions && res.body.extensions.cost) {
-              this.updateGraphqlLimits(res.body.extensions.cost);
-            }
-
-            if (Array.isArray(res.body.errors)) {
-              // Make Got consider this response errored and retry if needed.
-              throw new Error(res.body.errors[0].message);
-            }
-          }
-
-          return res;
-        }
-      ],
-      beforeError: [decorateError]
-    }
+    body: json ? this.options.stringifyJson({ query: data, variables }) : data
   };
+
+  const afterResponse = (res) => {
+    if (res.body) {
+      if (res.body.extensions && res.body.extensions.cost) {
+        this.updateGraphqlLimits(res.body.extensions.cost);
+      }
+
+      if (Array.isArray(res.body.errors)) {
+        // Make Got consider this response errored and retry if needed.
+        throw new Error(res.body.errors[0].message);
+      }
+    }
+
+    return res;
+  };
+
+  if (this.options.hooks) {
+    options.hooks = { ...this.options.hooks };
+    options.hooks.afterResponse = [afterResponse];
+    options.hooks.beforeError = [decorateError];
+
+    if (this.options.hooks.afterResponse) {
+      options.hooks.afterResponse.push(...this.options.hooks.afterResponse);
+    }
+
+    if (this.options.hooks.beforeError) {
+      options.hooks.beforeError.push(...this.options.hooks.beforeError);
+    }
+  } else {
+    options.hooks = {
+      afterResponse: [afterResponse],
+      beforeError: [decorateError]
+    };
+  }
+
+  if (this.options.maxRetries > 0) {
+    options.retry = {
+      calculateDelay,
+      errorCodes: retryableErrorCodesArray,
+      limit: this.options.maxRetries,
+      // Don't clamp Shopify `Retry-After` header values too low.
+      maxRetryAfter: Infinity,
+      methods: ['POST'],
+      statusCodes: retryableStatusCodesArray
+    };
+  } else {
+    options.retry = 0;
+  }
 
   return got(uri, options).then(responseData);
 };

test/shopify.test.js
@@ -702,6 +702,87 @@ describe('Shopify', () => {
         expect(result.name).equal('My Cool Test Shop');
       });
     }).timeout(10000);
+
+    it('calls hooks passed as options when accessing resources', () => {
+      function beforeRequest() {
+        beforeRequest.called = true;
+      }
+
+      function afterResponse(x) {
+        afterResponse.called = true;
+        return x;
+      }
+
+      const shopify = new Shopify({
+        accessToken,
+        shopName,
+        hooks: {
+          beforeRequest: [beforeRequest],
+          afterResponse: [afterResponse]
+        }
+      });
+
+      addWorkingRESTRequestMock(scope);
+
+      return shopify.shop.get().then(() => {
+        expect(beforeRequest.called).to.be.true;
+        expect(afterResponse.called).to.be.true;
+      });
+    });
+
+    it('calls hooks passed as options when making raw requests', () => {
+      function beforeRequest() {
+        beforeRequest.called = true;
+      }
+
+      function afterResponse(x) {
+        afterResponse.called = true;
+        return x;
+      }
+
+      const shopify = new Shopify({
+        accessToken,
+        shopName,
+        hooks: {
+          beforeRequest: [beforeRequest],
+          afterResponse: [afterResponse]
+        }
+      });
+
+      scope.get('/test').reply(200, {});
+
+      return shopify.request(url, 'GET').then(() => {
+        expect(beforeRequest.called).to.be.true;
+        expect(afterResponse.called).to.be.true;
+      });
+    });
+
+    it('calls the beforeRetry hook for retried requests', () => {
+      function beforeRetry() {
+        beforeRetry.called = true;
+      }
+
+      const shopify = new Shopify({
+        accessToken,
+        shopName,
+        maxRetries: 3,
+        hooks: {
+          beforeRetry: [beforeRetry]
+        }
+      });
+
+      scope.get('/admin/shop.json').replyWithError({
+        message: 'the network is broken',
+        code: 'ECONNRESET'
+      });
+
+      addWorkingRESTRequestMock(scope);
+
+      return shopify.shop.get().then((result) => {
+        expect(beforeRetry.called).to.be.true;
+        expect(result.name).equal('My Cool Test Shop');
+      });
+    });
   });
 
   describe('Shopify#graphql', () => {
@@ -1127,5 +1208,90 @@ describe('Shopify', () => {
           expect(result.shop.name).equal('My Cool Test Shop');
         });
     }).timeout(3000);
+
+    it('calls hooks passed as options when making graphql requests', () => {
+      function beforeRequest() {
+        beforeRequest.called = true;
+      }
+
+      function afterResponse(x) {
+        afterResponse.called = true;
+        return x;
+      }
+
+      const shopify = new Shopify({
+        accessToken,
+        shopName,
+        hooks: {
+          beforeRequest: [beforeRequest],
+          afterResponse: [afterResponse]
+        }
+      });
+
+      scope.post('/admin/api/graphql.json').reply(200, {
+        data: { foo: 'bar' }
+      });
+
+      return shopify.graphql('query').then(() => {
+        expect(beforeRequest.called).to.be.true;
+        expect(afterResponse.called).to.be.true;
+      });
+    });
+
+    it('calls the beforeRetry hook for retried requests', () => {
+      function beforeRetry() {
+        beforeRetry.called = true;
+      }
+
+      const shopify = new Shopify({
+        accessToken,
+        shopName,
+        maxRetries: 3,
+        hooks: {
+          beforeRetry: [beforeRetry]
+        }
+      });
+
+      scope.post('/admin/api/graphql.json').replyWithError({
+        message: 'the network is broken',
+        code: 'ECONNRESET'
+      });
+
+      addWorkingGraphQLRequestMock(scope);
+
+      return shopify.graphql('query { shop { id name } }').then(() => {
+        expect(beforeRetry.called).to.be.true;
+      });
+    });
+
+    it('calls the beforeError hook for errors', () => {
+      function beforeError(x) {
+        beforeError.called = true;
+        return x;
+      }
+
+      const shopify = new Shopify({
+        accessToken,
+        shopName,
+        hooks: {
+          beforeError: [beforeError]
+        }
+      });
+
+      const message = 'Something wrong happened';
+
+      scope.post('/admin/api/graphql.json').replyWithError(message);
+
+      return shopify.graphql('query { shop { id name } }').then(
+        () => {
+          throw new Error('Test invalidation');
+        },
+        (err) => {
+          expect(beforeError.called).to.be.true;
+          expect(err).to.be.an.instanceof(got.RequestError);
+          expect(err.message).to.equal(message);
+        }
+      );
+    });
   });
 });

commit 7ff7b4b42bbfb4810a8572031c2477dd08fb3bf3

Fix got types to properly reference the hooks type
This fixes a type error introduced in eeda19880b634d5080e3ab2cc5b77513276c8dbe and reported in #566 . I made this mistake by copying code suitable for got 12 into a codebase using got 11 -- my apologies!

index.d.ts
@@ -1,7 +1,7 @@
 // Type definitions for shopify-api-node
 // Project: shopify-api-node
 // Definitions by: Rich Buggy <rich@buggy.id.au>
-import { Got } from 'got';
+import { Hooks } from 'got';
 
 /*~ This is the module template file for class modules.
  *~ You should rename it to index.d.ts and place it in a folder with the same name as the module.
@@ -788,7 +788,7 @@ declare namespace Shopify {
     presentmentPrices?: boolean;
     shopName: string;
     timeout?: number;
-    hooks?: Got.Hooks;
+    hooks?: Hooks;
   }
 
   export interface IPrivateShopifyConfig {
@@ -800,7 +800,7 @@ declare namespace Shopify {
     presentmentPrices?: boolean;
     shopName: string;
     timeout?: number;
-    hooks?: Got.Hooks;
+    hooks?: Hooks;
   }
 
   export interface ICallLimits {