ec1393d3bd5689da3868d72912fdde21f3e3b104
Julius Plaras <juliusplaras@gmail.com>
2023-03-31 11:56:37 -0400

    Update metafield.js
    Adds new method to allow updating metafields by resource & resource ID.

--- resources/metafield.js
+++ resources/metafield.js
@@ -20,4 +20,20 @@ function Metafield(shopify) {
 
 assign(Metafield.prototype, base);
 
+/**
+ * Updates a metafield by ID, resource/resource ID.
+ *
+ * @param {Number} metaFieldId
+ * @param {String} resource
+ * @param {Number} resourceId
+ * @return {Promise} Promise that resolves with the result
+ * @public
+ */
+Metafield.prototype.updateV2 = function(metaFieldId, resource, resourceId, params) {
+  const urlString = `${resource}/${resourceId}/${this.name}`;
+  if (!this.name === urlString) this.name = urlString;
+  const url = this.buildUrl(metaFieldId);
+  return this.shopify.request(url, 'PUT', undefined, params);
+}
+
 module.exports = Metafield;
